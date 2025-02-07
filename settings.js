6a594ba14042616c01e28e26e023c2c2a19b70ce
jeet-5409 <uiuio6526@gmail.com>
2025-02-08 04:39:05 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,12 +1,12 @@
 const settings = {
   packname: 'Knight',
   author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botName: "Bot",
+  botOwner: 'Queen', // Your name
+  ownerNumber: '919330954192', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
-  description: "This is a bot for managing group commands and automating tasks.",
+  description: "This is a bot by me.",
   version: "1.0.0",
 };
 
